d96773e2e51f272087aca45c03d4ca88e8c8092b
JuMaLaB <julien.malaboeuf@gmail.com>
2022-02-01 16:11:42 +0100

    add authProvider

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './users';
 import { PostList, PostEdit, PostCreate } from "./post";
 import Dashboard from './Dashboard';
+import authProvider from './authProvider';
 
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-  <Admin dashboard={Dashboard} dataProvider={dataProvider} >
+  <Admin dashboard={Dashboard} dataProvider={dataProvider} authProvider={authProvider} >
     <Resource name="users" list={UserList} edit={EditGuesser} icon={UserIcon} />
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
   </Admin>
