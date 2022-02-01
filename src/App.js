dcf0e179a8d18509188f8f3dc6c72b0dc80239d0
JuMaLaB <julien.malaboeuf@gmail.com>
2022-02-01 16:01:53 +0100

    add custom home page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { Admin, EditGuesser, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './users';
 import { PostList, PostEdit, PostCreate } from "./post";
+import Dashboard from './Dashboard';
 
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-  <Admin dataProvider={dataProvider} >
+  <Admin dashboard={Dashboard} dataProvider={dataProvider} >
     <Resource name="users" list={UserList} edit={EditGuesser} icon={UserIcon} />
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
   </Admin>
