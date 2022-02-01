4138fa491bf41252aa0d8cd357a8f9a398035b0a
JuMaLaB <julien.malaboeuf@gmail.com>
2022-02-01 16:18:20 +0100

    add responsive design

--- src/post.js
+++ src/post.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useMediaQuery } from '@material-ui/core';
 import {
   List,
   Datagrid,
@@ -11,21 +12,37 @@ import {
   SelectInput,
   TextInput,
   Create,
+  SimpleList,
 } from 'react-admin';
 
-export const PostList = props => (
-  <List filters={postFilters} {...props} >
-    <Datagrid>
-      <TextField source="id" />
-      <ReferenceField source="userId" reference="users">
-        <TextField source="name" />
-      </ReferenceField>
-      <TextField source="title" />
-      <TextField source="body" />
-      <EditButton />
-    </Datagrid>
-  </List>
-);
+export const PostList = (props) => {
+
+  const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
+
+  return (
+    <List filters={postFilters} {...props} >
+
+      {isSmall ? (
+        <SimpleList
+          primaryText={record => record.title}
+          secondaryText={record => `${record.views} views`}
+          tertiaryText={record => new Date(record.published_at).toLocaleDateString()}
+        />
+      ) : (
+        <Datagrid>
+          <TextField source="id" />
+          <ReferenceField source="userId" reference="users">
+            <TextField source="name" />
+          </ReferenceField>
+          <TextField source="title" />
+          <TextField source="body" />
+          <EditButton />
+        </Datagrid>
+      )}
+
+    </List>
+  );
+};
 
 export const PostEdit = props => (
   <Edit title={<PostTitle />} {...props} >
